a45bb9763be625c39ea51f8dddbe21c104017875
ampham <ampersandhammond@mail.com>
2017-03-07 14:53:32 -0500

    Fixed Update create user_id

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -2,6 +2,7 @@ class UpdatesController < ApplicationController
 
 	def new
 		@update = Update.new
+		#@update.user_id = current_user.id
 
 		# make locations IDs available to select in the form
 		locations = Location.select(:id).distinct
@@ -12,8 +13,15 @@ class UpdatesController < ApplicationController
 	end
 
 	def create
+		locations = Location.select(:id).distinct
+		@locations_list = []
+		locations.each do |loc|
+			@locations_list << loc.id
+		end
+
 		@user = current_user
 		@update = Update.new(allowed_params)
+		@update.user_id = current_user.id
 
 		if @update.save
 			flash[:success] = "Created new update"
@@ -27,11 +35,10 @@ class UpdatesController < ApplicationController
 		@update = Update.find(params[:id])
 	end
 
-
 	private
 
 		def allowed_params
-			params.require(:update).permit(:lead, :mercury, :plutonium, :location_id)
+			params.require(:update).permit(:lead, :mercury, :plutonium, :location_id, :user_id)
 		end
 
 end
