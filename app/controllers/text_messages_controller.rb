30b253b24f85d00625e595f131e59aacf5b1140c
ampham <ampersandhammond@mail.com>
2017-03-08 19:51:03 -0500

    Trying again

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -1,17 +1,13 @@
 class TextMessagesController < ApplicationController
 
 	def send_text
-	  sender = params[:From]
-	  friends = {
-	    "+14153334444" => "Curious George",
-	    "+14158157775" => "Boots",
-	    "+14155551234" => "Virgil"
-	  }
-	  name = friends[sender] || "Mobile Monkey"
-	  twiml = Twilio::TwiML::Response.new do |r|
-	    r.Message "Hello, #{name}. Thanks for the message."
-	  end
-	  twiml.text	
+		content_type 'text/xml'
+
+		response = Twilio::TwiML::Response.new do |r|
+			r.message = 'Hey this is a message from the internet'
+		end
+
+		response.to_xml
 	end
-	
+
 end
