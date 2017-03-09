7a7bb3c0371f7e4ba706b58e7be3bc94d057488b
ampham <ampersandhammond@mail.com>
2017-03-08 19:40:28 -0500

    Trying again

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -1,19 +1,17 @@
 class TextMessagesController < ApplicationController
 
 	def send_text
-    number_to_send_to = params[:number_to_send_to]
-
-    twilio_sid = "PN43a9a5f3c5a4803bac1c6de8f206e6df"
-    twilio_token = "5646ec386444e72c0fc4b31cadbcb46c"
-    twilio_phone_number = "+13478945155"
-
-    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
-
-    @twilio_client.account.sms.messages.create(
-      :from => "+1#{twilio_phone_number}",
-      :to => number_to_send_to,
-      :body => "This is an message. It gets sent to #{number_to_send_to}"
-		)
+	  sender = params[:From]
+	  friends = {
+	    "+14153334444" => "Curious George",
+	    "+14158157775" => "Boots",
+	    "+14155551234" => "Virgil"
+	  }
+	  name = friends[sender] || "Mobile Monkey"
+	  twiml = Twilio::TwiML::Response.new do |r|
+	    r.Message "Hello, #{name}. Thanks for the message."
+	  end
+	  twiml.text	
 	end
-
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,5 @@ Rails.application.routes.draw do
     end
   end
 
-  post '/sms', to: 'text_messages#send_text'
+  get '/sms', to: 'text_messages#send_text'
 end
