258902ac1228dcdefc12c7b21db55d726432bd02
ampham <ampersandhammond@mail.com>
2017-03-08 20:00:01 -0500

    Trying again

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -1,7 +1,7 @@
 class TextMessagesController < ApplicationController
 
 	def send_text
-		content_type 'text/xml'
+		#content_type 'text/xml'
 
 		response = Twilio::TwiML::Response.new do |r|
 			r.message = 'Hey this is a message from the internet'
