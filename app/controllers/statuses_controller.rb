2604fb2e4eba240ea5529dd9d0fb0a22735de509
ampham <ampersandhammond@mail.com>
2017-03-09 14:51:12 -0500

    Statuses controller

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,2 +1,30 @@
 class StatusesController < ApplicationController
+
+	def new
+		@status = Status.new
+	end
+
+	def create
+		@user = current_user
+		@status = Status.new(allowed_params)
+		@status.user_id
+
+		if @status.save
+			flash[:success] = "Created new status"
+			redirect_to @status
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@status = Status.find(params[:id])
+	end
+
+	private
+
+		def allowed_params
+			params.require(:update).permit(:o2, :conductivity, :pH, :turbidity, :temp, :user_id, :location_id)
+		end
+
 end
