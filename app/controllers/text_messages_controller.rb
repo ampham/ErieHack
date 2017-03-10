784cd3db4119b7a3ced061cbd338c75df97a2430
ampham <ampersandhammond@mail.com>
2017-03-09 19:10:29 -0500

    Testing texting

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -12,7 +12,7 @@ class TextMessagesController < ApplicationController
 
 			response = Twilio::TwiML::Response.new do |r|
 				r.Sms "You have asked for data on #{location_name}." +
-				r.Sms "And something on a new line."
+				"\n\n\n And something on a new line."
 			end
 		# If the message starts with "POST" then we're saving a new update (DEPRECATED!)
 		elsif received_message.start_with?("POST")
