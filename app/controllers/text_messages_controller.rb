c8028b04d684c4ed1c316c849d0e198428ef0582
ampham <ampersandhammond@mail.com>
2017-04-01 11:23:08 -0400

    better_alert

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -42,7 +42,7 @@ class TextMessagesController < ApplicationController
 		@alert.notes = "Received"
 
 		@alert.save
-		reply = "You have reported \"#{@alert.message}\" for #{@alert.address}. Your " 
+		reply = "You have reported #{@alert.message} for #{@alert.address}. Your " 
 			+ "ticket number is #{@alert.id}. Thanks for using Waterwatcher."
 		
 		render json: { confirmation: reply }
