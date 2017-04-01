8964aed9258070d72b877c47449602ce0f675d1a
ampham <ampersandhammond@mail.com>
2017-04-01 12:10:58 -0400

    update_alert

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,6 +1,30 @@
 class AlertsController < ApplicationController
+	before_action :set_alert, only: [:edit, :show, :update]
 
 	def index
 		@alerts = Alert.all
+	end
+
+	def edit
+	end
+
+	def show
+	end
+
+	# TODO: Tighten up params in general
+	def update
+		if @alert.update(alert_params)
+			redirect_to alert_path(@alert), notice: 'Alert has been updated'
+		end
+	end
+
+	private
+
+	def set_alert
+		@alert = Alert.find(params[:id])
+	end
+
+	def alert_params
+		params.require(:alert).permit(:status)
 	end
 end
